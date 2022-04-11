ba01626b8171f07efcb59660eb40cd5c824e6ab2
shadowcat0202 <fxlimsin@gmail.com>
2022-04-11 20:24:00 +0900

    test titanic cnn

--- ComputerVision/Iris.py
+++ ComputerVision/Iris.py
@@ -3,7 +3,7 @@ from tensorflow.keras.layers import Dense
 from sklearn.preprocessing import LabelEncoder  # 문자열로 되어있는 클래스를 숫자 형태로 바꿔주는 작업(One-hot encoding)
 from keras.utils import np_utils
 
-import seaborn as sns
+# import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -32,42 +32,7 @@ model = Sequential()
 model.add(Dense(16, input_dim=4, activation='relu'))
 model.add(Dense(3, activation='softmax'))
 
-"""
-손실함수(loss=)의 종류
 
-Mean_Squared_error
-예측한 값과 실제 값 사이의 평균 제곱 오차를 정의한다. 공식이 매우 간단하며, 차가 커질수록 제곱 연산으로 인해서 값이 더욱 뚜렷해진다. 그리고 제곱으로 인해서 오차가 양수이든 음수이든 누적 값을 증가시킨다.
-
-RMSE(Root Mean Squared Error)
-MSE에 루트(√)를 씌운 것으로 MSE와 기본적으로 동일하다. MSE 값은 오류의 제곱을 구하기 때문에 실제 오류 평균보다 더 커지는 특성이 있어 MSE에 루트를 씌운 RMSE 은 값의 왜곡을 줄여준다.
-
-binary_crossentropy
-실제 레이블과 예측 레이블 간의 교차 엔트로피 손실을 계산한다. 레이블 클래스(0, 1로 가정)가 2개만 존재할 때
-Binary Crossentropy를 사용하면 좋다. 
-
-categorical_crossentropy
-다중 분류 손실함수로 출력값이 one-hot encoding 된 결과로 나오고 실측 결과와의 비교시에도 실측 결과는 
-one-hot encoding 형태로 구성된다.
-예를 들면 출력 실측값이 아래와 같은 형태(one-hot encoding)로 만들어 줘야 하는 과정을 거쳐야 한다.
-[[0 0 1]
- [0 1 0]
- [1 0 0]]  (배치 사이즈 3개인 경우)
-네트웍 레이어 구성시 마지막에 Dense(3, activation='softmax') 로 3개의 클래스 각각 별로 positive 확률값이 나오게 된다.
-[0.2, 0.3, 0.5]
-위 네트웍 출력값과 실측값의 오차값을 계산한다.
-
-sparse_categorical_crossentropy
-'categorical_entropy'처럼 다중 분류 손실함수이지만, 샘플 값은 정수형 자료이다. 예를 들어, 샘플 값이 아래와 같은 형태일 수 있다. (배치 사이즈 3개)
-[0, 1, 2] 
-네트웍 구성은 동일하게 Dense(3, activation='softmax')로 하고 출력값도 3개가 나오게 된다.
-즉, 샘플 값을 입력하는 부분에서 별도 원핫 인코딩을 하지 않고 정수값 그대로 줄 수 있다. 이런 자료를 사용할 때, 컴파일 단계에서 손실 함수만  'sparse_categorical_crossentropy'로 바꿔주면 된다.
-
-그 외
-mean_absolute_error / mean_absolute_percentage_error 
-mean_squared_logarithmic_error / cosine_proximity
-squared_hinge / hinge / categorical_hinge 
-logcosh / kullback_leibler_divergence / poisson 
-"""
 model.compile(loss="categorical_crossentropy",
               optimizer="adam",
               metrics=["accuracy"])
---
+++ ComputerVision/titan.py
@@ -0,0 +1,106 @@
+import pandas as pd
+import numpy
+import tensorflow as tf
+
+df_train = pd.read_csv("./database/train.csv")
+
+seed = 3
+numpy.random.seed(0)
+tf.random.set_seed(seed)
+
+df_train = df_train.drop(["Name", "PassengerId", "Ticket", "Fare", "Cabin"], axis=1)
+df_train["Age"].fillna(df_train["Age"].mean(), inplace=True)
+df_train["Embarked"] = df_train["Embarked"].fillna("S")  # 가장 많은 S로 통일
+df_train["Sex"] = df_train["Sex"].map({"male": 0, "female": 1})
+df_train["Embarked"] = df_train["Embarked"].map({"Q": 0, "C": 1, "S": 2})
+family = []
+for i in range(len(df_train)):
+    if df_train.loc[i, "SibSp"] >= 4:
+        family.append(2)
+    elif df_train.loc[i, "SibSp"] >= 2:
+        family.append(1)
+    else:
+        family.append(0)
+df_train["Family"] = family
+X = df_train.drop(["Survived"], axis=1)
+y = df_train["Survived"]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+
+## Functional
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Model
+
+# 각 층 셋팅
+# 입력층 (shape=(열,행)) 행:데이터는 계속 추가할수 있으니까 필요한건 column vector가 몇개 있는지가 중요하다
+
+inputs = Input(shape=(7,))
+"""
+from tensorflow import TensorShape
+inputs = Input()
+inputs.shape = TensorShape([None, 60])
+"""
+hidden = Dense(24, activation='relu')(inputs)
+hidden = Dense(10, activation='relu')(inputs)
+output = Dense(1, activation='relu')(hidden)
+f_logistic_model = Model(inputs=inputs, outputs=output)
+
+# 모델 컴파일
+f_logistic_model.compile(loss='binary_crossentropy',
+                         optimizer='adam',
+                         metrics=['accuracy'])
+# f_logistic_model.optimizer.lr = 0.001
+
+# 학습
+with tf.device("/device:GPU:0"):
+    f_logistic_model.fit(x=X, y=y, epochs=20, batch_size=5)
+    # verbose=False 하면 막대바 안보임
+# 모델 저장
+from keras.models import load_model
+f_logistic_model.save('my_model.h5')
+# del f_logistic_model    # 테스트를 위해 메모리 내의 모델을 삭제
+# f_logistic_model = load_model('./my_model.h5')
+
+df_test = pd.read_csv('./database/test.csv')
+pid = df_test["PassengerId"]  # [!!!]submit.csv 를 만들어줄 때 PassengerId가 필요하기 때문에 drop하기 전에 저장해둔다
+# predict하기 위한 test data도 columns를 동일하게 맞춰주어야 한다
+df_test = df_test.drop(["Name", "PassengerId", "Ticket", "Fare", "Cabin"], axis=1)
+df_test["Age"].fillna(df_test["Age"].mean(), inplace=True)
+df_test["Embarked"] = df_test["Embarked"].fillna("S")
+df_test["Sex"] = df_test["Sex"].map({"male": 0, "female": 1})
+df_test["Embarked"] = df_test["Embarked"].map({"Q": 0, "C": 1, "S": 2})
+family = []
+for i in range(len(df_test)):
+    if df_test.loc[i, "SibSp"] >= 4:
+        family.append(2)
+    elif df_test.loc[i, "SibSp"] >= 2:
+        family.append(1)
+    else:
+        family.append(0)
+df_test["Family"] = family
+test_result = f_logistic_model.predict(df_test, batch_size=32)
+
+submit = pd.DataFrame({"PassengerId": pid, "Survived": test_result})
+
+# index=False를 안하면 csv파일 0번쨰 columns에 index를 붙여준다 즉 제출용에 맞게 설정해주어야 한다
+submit.to_csv("./database/submit.csv", index=False)
+
+
+# model.evaluate() = [오차율?, 예측율]
+# score = f_logistic_model.evaluate(X_test, y_test, verbose=1)
+# print(f"정답률={score[1]*100} loss={score[0]}")
+
+df_perfect = pd.read_csv("./database/wow.csv")
+submit = pd.read_csv("./database/submit.csv")
+
+hit = 0
+miss = 0
+for i in range(len(test_result)):
+    if submit["Survived"][i] == df_perfect["Survived"][i]:
+        hit += 1
+    else:
+        miss += 1
+
+print(hit, miss, round(hit / (hit + miss), 4))
